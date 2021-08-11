b449377b757cbeb805296594dd1a7b42ef5d81e0
albertoDM13 <jesus_duk_12@hotmail.com>
2021-08-10 21:09:31 -0500

    2 files modified

--- src/app/input.component.ts
+++ src/app/input.component.ts
@@ -4,7 +4,11 @@ import { Component, Input } from '@angular/core';
   selector: 'pass',
   template: `
     <div class="div"></div>
-    <div class="div pos"><input type="password" class="input" /></div>
+    <div class="mod">
+      <div class="ltr"></div>
+      <div class="div pos"><input type="password" class="input" /></div>
+      <div class="ltr"></div>
+    </div>
     <div class="div"></div>
   `,
   styleUrls: [`./app.component.css`]
